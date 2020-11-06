f34ccd89562cb150100b4378863899a889029de9
Ahmad Gad <70626969+iAhmadGad@users.noreply.github.com>
2020-11-06 19:43:47 +0300

    Add files via upload

---
+++ src/Cal/GeoGenius.java
@@ -0,0 +1,14 @@
+package Cal;
+
+import java.util.Scanner;
+public class GeoGenius {
+
+	static Scanner read = new Scanner(System.in);
+	public static void main(String[] args) {
+		
+		System.out.println();
+		
+
+	}
+
+}
