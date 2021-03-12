478786129f80c5806ec9b19694adbbf88ff0975b
Ahmad Gad <70626969+iAhmadGad@users.noreply.github.com>
2021-03-12 22:36:47 +0300

    Add files via upload

---
+++ src/v1/script/MathOperations.java
@@ -0,0 +1,41 @@
+package script;
+
+public class MathOperations {
+	
+	//addition operation
+	public int add(int onum1, int onum2)
+	{
+		
+		int oresult = onum1 + onum2;
+		return oresult;
+		
+	}
+	
+	//subtract operation
+	public int sub(int onum1, int onum2)
+	{
+		
+		int oresult = onum1 - onum2;
+		return oresult;
+		
+	}
+	
+	//multiply operation
+	public int mtp(int onum1, int onum2)
+	{
+		
+		int oresult = onum1 * onum2;
+		return oresult;
+		
+	}
+	
+	//divide operation
+	public int dvd(int onum1, int onum2)
+	{
+		
+		int oresult = onum1 / onum2;
+		return oresult;
+		
+	}
+	
+}
